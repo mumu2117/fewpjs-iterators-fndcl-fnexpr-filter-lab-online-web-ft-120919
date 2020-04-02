f3dc55d8ce658e5d14bce08d4b1d8dd16a90a4c8
mumu2117 <jmillerarcher@me.comgit config --global user.name mumu2117>
2020-04-02 14:16:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(n => {
+      return n.toLowerCase() === name.toLowerCase();
+    })
+  }
+  
+  function fuzzyMatch (list, partialName) {
+    let lengthOfName = partialName.length;
+    return list.filter(function (driverName) {
+      return driverName.slice(0, lengthOfName) === partialName;
+    });
+  }
+  
+  function matchName(drivers, name){
+    return drivers.filter(n => {
+      return n.name.toLowerCase() === name.toLowerCase();
+    })
+  }
+  
